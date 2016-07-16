3b424efc3f4fd861e99879be5a539b8536e0e23e
expeehaa <extra1503@gmx.de>
2016-07-16 13:06:56 +0200

    filled de.expeehaa.spigot.Wands with override method for onEnable and onDisable

--- src/main/java/de/expeehaa/spigot/Wands.java
+++ src/main/java/de/expeehaa/spigot/Wands.java
@@ -1,5 +1,16 @@
 package de.expeehaa.spigot;
 
-public class Wands {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Wands extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
 }
